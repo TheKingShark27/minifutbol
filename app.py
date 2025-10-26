cab39eb9fd1e97fec094b06c7b5d4bdb90b3c6b2
TheKingShark27 <tgoitia27@gmail.com>
2025-10-26 00:14:48 -0700

    Higher V Low

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import *
-
+from data import teams
+from random import randint
+data = teams
 
 app = Flask(__name__)
 
@@ -9,7 +11,26 @@ def home():
 
 @app.route('/main',methods = ['GET', 'POST'])
 def main():
-    return render_template('main.html')
+    c=0
+    team1=''
+    team2=''
+    num=[]
+    for i in range(50):
+        num.append(randint(1,14))
+    action = request.form.get('main')
+    if action=='left':
+        if data[num[0]]['number'] > data[num[4]]['number']:
+            return "That is correcto mundo"
+        else:
+            return "You is wrong homes"
+    elif action=='right':
+        if data[num[0]]['number'] < data[num[4]]['number']:
+            return "You is correcto Mundo"
+        else:
+            return "You is wrong homes"
+        
+    
+    return render_template('main.html',teams=data, count=c, t1=team1, t2=team2,nums=num)
 
 
 
---
+++ data.py
@@ -0,0 +1,20 @@
+#fake team values for testing purposes until I can get an API with teams and other data
+
+teams = {
+    1: {"name": "Barcelona", "number": 450},
+    2: {"name": "Real Madrid", "number": 500},
+    3: {"name": "Real Betis", "number": 200},
+    4: {"name": "Arsenal", "number": 400},
+    5: {"name": "Liverpool", "number": 600},
+    6: {"name": "Cruz Azul", "number": 100},
+    7: {"name": "America", "number": 10},
+    8: {"name": "Atletico Madrid", "number": 300},
+    9: {"name": "West Ham United", "number": 400},
+    10: {"name": "Manchester United", "number": 412},
+    11: {"name": "Chelsea", "number": 529},
+    12: {"name": "Inter Milan", "number": 459},
+    13: {"name": "AC Milan", "number": 391},
+    14: {"name": "Juventus", "number": 467}
+}
+
+
